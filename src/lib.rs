29fc55cf861c9c63450637dd455974eb38a0fa66
Marco Bacis <marc.bacis@gmail.com>
2025-04-05 19:15:03 +0200

    feat: wrap proto objects, add send_template

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,5 @@
 use base64::Engine;
-use pkg::kannon::mailer::{
-    apiv1::{SendHtmlReq, mailer_client::MailerClient},
-    types::{Recipient, Sender},
-};
+use pkg::kannon::mailer::apiv1::{mailer_client::MailerClient, SendHtmlReq, SendTemplateReq};
 use thiserror::Error;
 use std::collections::HashMap;
 use tonic::{
@@ -28,6 +25,52 @@ pub enum Error {
     ConnectionError(#[from] tonic::transport::Error), 
     #[error("Error while sending mail")]
     SendMailError(#[from] Status)
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct Sender {
+    pub email: String,
+    pub alias: String,
+}
+
+impl Into<pkg::kannon::mailer::types::Sender> for Sender {
+    fn into(self) -> pkg::kannon::mailer::types::Sender {
+        pkg::kannon::mailer::types::Sender {
+            email: self.email,
+            alias: self.alias,
+        }
+    }
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct Recipient {
+    pub email: String,
+    pub fields: HashMap<String, String>,
+}
+
+impl Into<pkg::kannon::mailer::types::Recipient> for Recipient {
+    fn into(self) -> pkg::kannon::mailer::types::Recipient {
+        pkg::kannon::mailer::types::Recipient {
+            email: self.email,
+            fields: self.fields,
+        }
+    }
+}
+
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct Attachment {
+    pub filename: String,
+    pub content: Vec<u8>,
+}
+
+impl Into<pkg::kannon::mailer::apiv1::Attachment> for Attachment {
+    fn into(self) -> pkg::kannon::mailer::apiv1::Attachment {
+        pkg::kannon::mailer::apiv1::Attachment {
+            filename: self.filename,
+            content: self.content,
+        }
+    }
 }
 
 pub struct Kannon {
@@ -58,14 +101,14 @@ impl Kannon {
         format!("Basic {}", token)
     }
 
-    pub async fn send_email(&mut self, recipients: Vec<Recipient>, subject: String, body: String) -> Result<(), Error> {
+    pub async fn send_email(&mut self, recipients: Vec<Recipient>, subject: String, body: String, attachments: Vec<Attachment>) -> Result<(), Error> {
         let mut request = Request::new(SendHtmlReq {
-            sender: Some(self.sender.clone()),
+            sender: Some(self.sender.clone().into()),
             subject: subject,
             html: body,
             scheduled_time: None,
-            recipients: recipients,
-            attachments: vec![],
+            recipients: recipients.into_iter().map(Recipient::into).collect(),
+            attachments: attachments.into_iter().map(Attachment::into).collect(),
             global_fields: HashMap::new(),
         });
     
@@ -75,6 +118,27 @@ impl Kannon {
             .insert(MetadataKey::from_static("authorization"), metadata_value);
     
         self.client.send_html(request).await?;
+
+        Ok(())
+    }
+
+    pub async fn send_template(&mut self, recipients: Vec<Recipient>, subject: String, template_id: String, attachments: Vec<Attachment>) -> Result<(), Error> {
+        let mut request = Request::new(SendTemplateReq {
+            sender: Some(self.sender.clone().into()),
+            subject: subject,
+            template_id: template_id,
+            scheduled_time: None,
+            recipients: recipients.into_iter().map(Recipient::into).collect(),
+            attachments: attachments.into_iter().map(Attachment::into).collect(),
+            global_fields: HashMap::new(),
+        });
+    
+        let metadata_value = self.get_auth_header().try_into().unwrap();
+        request
+            .metadata_mut()
+            .insert(MetadataKey::from_static("authorization"), metadata_value);
+    
+        self.client.send_template(request).await?;
 
         Ok(())
     }
